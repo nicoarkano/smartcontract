c2e6b65855a14a743c88ef990bd8ce78c4f4f719
nicoarkano <nicoarkano@gmail.com>
2020-09-27 13:12:58 -0300

    Agrego lenguaje solidity

---
+++ helloworld.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.0;
+    contract HelloWorldContract {
+        string word = "Hello World"; 
+            function getWord () constant returns(string) {
+                return word;   
+            }
+    }
---
+++ variables.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.0;
+
+    contract Variables {
+        string public one = "one";
+        string two = "two";
+        int public three = 3;
+        uint public  four =4;
+        address myAdress= 0x7A6479A578Fe54Ef82254c794F4368E462F85248;
+    
+    }
